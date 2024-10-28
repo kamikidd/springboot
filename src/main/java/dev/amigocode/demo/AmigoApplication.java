9886d996ff762f583e85793ab0d206e402465a41
Youyou <puyouyou@gmail.com>
2024-10-28 16:15:36 +0100

    local data List

--- src/main/java/dev/amigocode/demo/AmigoApplication.java
+++ src/main/java/dev/amigocode/demo/AmigoApplication.java
@@ -1,19 +1,54 @@
 package dev.amigocode.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class AmigoApplication {
+    private static List<Customer> customers;
+    static {
+        customers= new ArrayList<>();
+        Customer alex= new Customer(
+                1,
+                "Alex",
+                "alex@gmail.com",
+                21
+        );
+
+        customers.add(alex);
+        Customer jamila= new Customer(
+                2,
+                "jamila",
+                "jamila@gmail.com",
+                19
+        );
+
+        customers.add(jamila);
+    }
+
     public static void main(String[] args) {
+        System.out.println(customers);
         SpringApplication.run(AmigoApplication.class,args);
     }
 
-    @GetMapping("/greet")
-    public String greet() {
-        return "Hello World";
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class Customer {
+        private Integer id;
+        private String name;
+        private String email;
+        private Integer age;
+
     }
+
 }
