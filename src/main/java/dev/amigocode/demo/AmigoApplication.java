9136f6e3979819da56e4cfdac8abc17e1d947812
Youyou <puyouyou@gmail.com>
2024-10-29 10:11:36 +0100

    get single customer

--- src/main/java/dev/amigocode/demo/AmigoApplication.java
+++ src/main/java/dev/amigocode/demo/AmigoApplication.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -36,8 +37,16 @@ public class AmigoApplication {
     }
 
     public static void main(String[] args) {
-        System.out.println(customers);
         SpringApplication.run(AmigoApplication.class,args);
+    }
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomers(@PathVariable("customerId") Integer customerId) {
+         return customers.stream().filter(c -> c.id.equals(customerId)).findFirst().orElseThrow(()->new IllegalArgumentException("customer with id [%s] not found".formatted(customerId)));
     }
 
     @Data
